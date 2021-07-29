f83d519833ee0ac90e82367f1f2bd05d2d75d0d6
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-07-29 09:11:49 -0700

    Added button to cycle through color variants of items

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 const ItemCard = ({ item }) => {
+    const [variantNmbr, setVariantNmbr] = useState(0);
+
+    //console.log(item);
     return(
-        <div className='item-card' key={item[0]['internal-id']}>
+        <div className='item-card' key={item[variantNmbr]['internal-id']}>
             <p>{item[0]['name']['name-USen']}</p>
-            <img src={item[0]['image_uri']} alt='Furniture'></img>
+            <img src={item[variantNmbr]['image_uri']} alt='Furniture'></img>
+            <p>Variant No. {variantNmbr}</p>
+
+            <button onClick={() => {
+                    if (item.length-1 === variantNmbr) setVariantNmbr(0);
+                    else setVariantNmbr(variantNmbr+1);
+                }}>Cycle Variants</button>
         </div>
     );
 }
