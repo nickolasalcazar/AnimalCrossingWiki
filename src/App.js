119e789efededf1d444832c755d14b718e10a7fc
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-09-23 09:10:55 -0700

    Added logo for the app, as well as folder for media

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 //import ItemList from "./components/ItemList";
 import ItemCatalog from "./components/ItemCatalog";
+import logo from "./media/CritterCatalogLogo.png";
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt="CritterCatalogLogo"></img>
       {/* <ItemList /> */}
       <ItemCatalog />
     </div>
