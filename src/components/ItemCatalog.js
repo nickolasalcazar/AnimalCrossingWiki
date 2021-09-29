ad3e2af94c1463402557906f995dea248e6db611
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-09-28 20:40:02 -0700

    Reworked fetch requests of items

--- src/components/ItemCatalog.js
+++ src/components/ItemCatalog.js
@@ -10,11 +10,67 @@ const ItemCatalog = () => {
     const {data, isPending, error} = useFetchGET('http://acnhapi.com/v1/houseware/');
 
     const [items, setItems] = useState([]);
+    const [itemsT, setItemsT] = useState([]);   // New version of items
+
     const [itemsPerPage, /*setItemsPerPage*/] = useState(30);
     const [lastItemIndex, setLastItemIndex] = useState(itemsPerPage);
 
     useEffect(() => {
-        if (!isPending) setItems(Object.values(data));
+        if (!isPending) setItems(Object.values(data)); // ORIGINAL
+
+        if (!isPending) {
+            let allItems = Object.values(data);
+            // console.log(allItems);
+
+            // For every item in allItems
+            for (let i=0; i<allItems.length; i++) {
+                let variants = [];
+
+                // For every variant of that item
+                for (let j=0; j<allItems[i].length; j++) {
+                    let variant = allItems[i][j];
+
+                    let obj = {
+                        // 'Type': 'furniture',
+                        'Buy Price': variant['buy-price'],
+                        'Customizable Body': variant['canCustomizeBody'],
+                        'Customizable Pattern': variant['canCustomizePattern'],
+                        'Colors': [variant['color-1'], variant['color-2']],
+                        'file-name': variant['file-name'],
+                        'HHA Concepts': [variant['hha-concept-1'],
+                            variant['hha-concept-2']],
+                        'HHA Series': variant['hha-series'],
+                        'HHA Set': variant['hha-set'],
+                        'Image': variant['image_uri'],
+                        'internal-id:': variant['internal_id'],
+                        'Nook Catalog': variant['isCatalog'],
+                        'DIY': variant['isDIY'],
+                        'Interactive': variant['isInteractive'],
+                        'Outdoor': variant['isOutdoor'],
+                        'Kit Cost': variant['kit-cost'],
+                        'Lighting Type': variant['lighting-type'],
+                        'Name': variant['name']['name-USen'],
+                        'Pattern': variant['pattern'],
+                        'Pattern Title': variant['pattern-title'],
+                        'Sell Price': variant['sell-price'],
+                        'Size': variant['size'],
+                        'Source': variant['source'],
+                        'Source Detail': variant['source-detail'], // How to obtain
+                        'Speaker': variant['speaker-type'],
+                        'Tag': variant['tag'], // One word description
+                        'Variant': variant['variant'], // Name of variant
+                        'variant-id': variant['variant-id'],
+                        'version': variant['version']
+                    }
+                    variants.push(obj);
+                }
+                // console.log(obj);
+
+                // Now push variants to all items
+                setItemsT(oldState => [...oldState, variants]);
+            }
+            // console.log(itemsT); // itemT works
+        };
     }, [data, isPending]);
 
     return (
