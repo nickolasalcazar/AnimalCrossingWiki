e9da90aafdcd92552a842bd947be025728270c7a
Nickolas <nickolasalcazar@gmail.com>
2022-11-09 21:59:37 -0800

    Modified Catalog HTML

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import useFetchGET from "../../hooks/useFetchGET";
 
-import Hero from "../../components/Hero/Hero";
 import FurnitureFilter from "../../pages/Furniture/FurnitureFilter/FurnitureFilter";
 import Pagination from "../../components/Pagination/Pagination";
 import ItemList from "../../components/ItemList/ItemList";
@@ -30,44 +29,35 @@ function Catalog({ type = null }) {
   }, [data, isPending]);
 
   return (
-    // <main className="bug-catalog-wrapper">
-    <main className="catalog-wrapper">
-      {/* Hero should not be included in Catalog, semantically */}
-      {/* <Hero title="bug" image="/assets/wallpapers/villagers.jpg" /> */}
-      {/* <div className="bug-catalog"> */}
-      <div className="catalog">
-        {error && <p>Something went wrong...</p>}
-        {isPending && <p>Loading...</p>}
-        {!isPending && (
-          <div>
-            {type === "houseware" && (
-              <FurnitureFilter
-                items={Object.values(data)}
-                setItems={setItems}
-              />
-            )}
-            <Pagination
-              totalNumberOfItems={items.length}
-              itemsPerPage={itemsPerPage}
-              lastItemIndex={lastItemIndex}
-              setLastItemIndex={setLastItemIndex}
-            />
-            <ItemList
-              listType={type}
-              items={items}
-              itemsPerPage={itemsPerPage}
-              lastItemIndex={lastItemIndex}
-            />
-            <Pagination
-              totalNumberOfItems={items.length}
-              itemsPerPage={itemsPerPage}
-              lastItemIndex={lastItemIndex}
-              setLastItemIndex={setLastItemIndex}
-            />
-          </div>
-        )}
-      </div>
-    </main>
+    <div className="catalog">
+      {error && <p>Something went wrong...</p>}
+      {isPending && <p>Loading...</p>}
+      {!isPending && (
+        <div>
+          {type === "houseware" && (
+            <FurnitureFilter items={Object.values(data)} setItems={setItems} />
+          )}
+          <Pagination
+            totalNumberOfItems={items.length}
+            itemsPerPage={itemsPerPage}
+            lastItemIndex={lastItemIndex}
+            setLastItemIndex={setLastItemIndex}
+          />
+          <ItemList
+            listType={type}
+            items={items}
+            itemsPerPage={itemsPerPage}
+            lastItemIndex={lastItemIndex}
+          />
+          <Pagination
+            totalNumberOfItems={items.length}
+            itemsPerPage={itemsPerPage}
+            lastItemIndex={lastItemIndex}
+            setLastItemIndex={setLastItemIndex}
+          />
+        </div>
+      )}
+    </div>
   );
 }
 
