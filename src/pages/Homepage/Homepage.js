94eaf93f67519ea5f1fe07ef0e3655e1152232f7
Nickolas <nickolasalcazar@gmail.com>
2022-12-12 19:30:32 -0800

    Added styling and content to homepage

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -31,34 +31,58 @@ function Homepage(props) {
           </p>
         </section>
       </article>
-      <h2 className="explore-by-category">Explore by Category:</h2>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/furniture">Furniture</Link>
-          </li>
-          <li>
-            <Link to="/villagers">Villagers</Link>
-          </li>
-          <li>
-            <Link to="/critters/bugs">Bugs</Link>
-          </li>
-          <li>
-            <Link to="/critters/fish">Fish</Link>
-          </li>
-          <li>
-            <Link to="/art">Art</Link>
-          </li>
-          <li>
-            <Link to="/Fossils">Fossils</Link>
-          </li>
-        </ul>
-      </nav>
-      <article>
-        <h2>About</h2>
+      <article className="explore-by-category-wrapper">
+        <h2 className="explore-by-category">Explore by Category</h2>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/furniture">
+                Furniture
+                <img src="/assets/categories/furniture.png" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/villagers">
+                Villagers
+                <img src="/assets/categories/villagers.png" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/critters/bugs">
+                Bugs
+                <img src="/assets/categories/bugs.png" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/critters/fish">
+                Fish
+                <img src="/assets/categories/fish.png" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/art">
+                Art
+                <img src="/assets/categories/art.png" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/Fossils">
+                Fossils
+                <img src="/assets/categories/fossils.png" />
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </article>
       <article>
-        <h2>Credit</h2>
+        <h2>About</h2>
+        <section>
+          <p>
+            This site is a hobby project. It was made using React.js and fetches
+            data from <a href="https://acnhapi.com/">ACNH API</a>, a publicly
+            available, unofficial Animal Crossing API.
+          </p>
+        </section>
       </article>
     </main>
   );
