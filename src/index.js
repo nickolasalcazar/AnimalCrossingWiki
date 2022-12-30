8f1a6728303a0c03830ab0a57608b7961f8ec15e
Nickolas <nickolasalcazar@gmail.com>
2022-12-30 10:36:01 -0800

    Changed BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 
-const CONFIG = require("./config.json");
+// const CONFIG = require("./config.json");
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={CONFIG.BASENAME}>
+    {/* <BrowserRouter basename={CONFIG.BASENAME}> */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
