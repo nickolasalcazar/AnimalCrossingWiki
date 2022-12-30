7aa5dcab7052f4bcabd8c391cfa5da05a6ebda96
Nickolas <nickolasalcazar@gmail.com>
2022-12-30 12:57:08 -0800

    Renabled BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-// import { HashRouter as BrowserRouter } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 
 const CONFIG = require("./config.json");
 
