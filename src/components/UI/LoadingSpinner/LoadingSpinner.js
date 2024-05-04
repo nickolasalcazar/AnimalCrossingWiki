2434622dc8cc1be3802717ea48e6243ab5cd0fd1
Nickolas <nickolasalcazar@gmail.com>
2024-05-04 11:40:39 -0700

    Modified loading message

--- src/components/UI/LoadingSpinner/LoadingSpinner.js
+++ src/components/UI/LoadingSpinner/LoadingSpinner.js
@@ -12,7 +12,8 @@ export default function LoadingSpinner() {
       <img src="assets/categories/furniture.png" />
       <h2>Loading...</h2>
       <p>
-        Initial loading time may be delayed as the server needs time to wake up.
+        The server may be asleep and needs time to awaken. Load times are
+        &lt;=30 seconds.
       </p>
     </div>
   );
